f12f1ffa0666cec0d5eb2d60202463179af7511d
subhash1717 <yadavsubhash1717@gmail.com>
2026-07-10 23:27:23 +0530

    frontend compleated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
+import HomePage from './landing_page/home/Homepage';
+import Signup from "./landing_page/signup/Signup";
+import AboutPage from './landing_page/about/Aboutpage';
+import ProductPage from './landing_page/product/ProductPage';
+import PricingPage from './landing_page/pricing/PricingPage';
+import SupportPage from './landing_page/support/SupportPage';
+import NotFound from './landing_page/NotFound';
+import Navbar from './landing_page/Navbar';
+import Footer from './landing_page/Footer';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <div>
-      <h1>
-        Hello!
-      </h1>
-    </div>
-  </React.StrictMode>
+  <BrowserRouter>
+  <Navbar />
+  <Routes>
+    <Route path="/" element={<HomePage />}></Route>
+    <Route path="/signup" element={<Signup />}></Route>
+    <Route path="/about" element={<AboutPage />}></Route>
+    <Route path="/product" element={<ProductPage />}></Route>
+    <Route path="/pricing" element={<PricingPage />}></Route>
+    <Route path="/support" element={<SupportPage />}></Route>
+    <Route path="*" element={<NotFound />}></Route>
+  </Routes>
+  <Footer />
+  </BrowserRouter>
 );
