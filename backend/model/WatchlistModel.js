33d9b3889c45ab860682a7f3d4374ed542e1f05a
subhash1717 <yadavsubhash1717@gmail.com>
2026-07-11 11:52:48 +0530

    I made some mistake to create schema so i fix it

--- backend/model/WatchlistModel.js
+++
@@ -1,7 +0,0 @@
-const { model } = require("mongoose");
-
-const { WatchlistSchema } = require('../schemas/WatchlistSchema');
-
-const WatchlistSchema = new model("watchlist", WatchlistSchema);
-
-module.exports = { HoldingsSchema };
